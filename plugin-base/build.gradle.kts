c5aab4b15f2a08dbf12b02bc906d0b1791c87ebc
chengzis <837650313@qq.com>
2025-10-23 17:35:19 +0800

    feat(gradle): 优化gradle脚本

--- plugin-base/build.gradle.kts
+++ plugin-base/build.gradle.kts
@@ -1,23 +1,18 @@
 plugins {
-    `java-gradle-plugin`
     `kotlin-dsl`
     `maven-publish`
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-    withJavadocJar()
-    withSourcesJar()
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+
+apply(from = rootProject.file("gradle/library.gradle"))
+apply(from = rootProject.file("gradle/publish.gradle"))
+
+
+// 依赖配置
+dependencies {
+    implementation("org.yaml:snakeyaml:2.2")
 }
 
-// 配置发布
 publishing {
-
     publications {
         create<MavenPublication>("pluginMaven") {
             groupId = property("project.groupId").toString()
@@ -25,14 +20,4 @@ publishing {
             version = property("project.version").toString()
         }
     }
-}
-
-
-// 依赖配置
-dependencies {
-    implementation(gradleApi())
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
-    implementation("com.android.tools.build:gradle:8.1.0") // 如果需要与Android构建交互
-
-    implementation("org.yaml:snakeyaml:2.2")
 }
--- plugin-starter/build.gradle.kts
+++ plugin-starter/build.gradle.kts
@@ -1,47 +1,9 @@
 plugins {
-    `java-gradle-plugin`
     `kotlin-dsl`
-    `maven-publish`
-    id("com.gradle.plugin-publish") version "2.0.0" // 可选，用于发布到Gradle插件门户
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-    withJavadocJar()
-    withSourcesJar()
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
 }
 
+apply(from = rootProject.file("gradle/plugin.library.gradle"))
 
-val groupId = property("project.groupId").toString()
-val artifactId = property("project.artifactId").toString()
-
-group = "$groupId.$artifactId"
-version = "1.0.0"
-
-gradlePlugin {
-    plugins {
-        val name = property("project.name").toString()
-        create(name) {
-            id = group.toString()
-            implementationClass = "$group.$name"
-            displayName = name
-            description = property("project.description").toString()
-        }
-    }
-}
-
-// 配置发布
-publishing {
-    repositories {
-        // 发布到 Maven Local (测试用)
-        mavenLocal()
-    }
-}
 
 // 配置签名 (发布到Maven Central必需)
 //signing {
@@ -53,22 +15,7 @@ publishing {
 
 // 依赖配置
 dependencies {
-    implementation(gradleApi())
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
-    implementation("com.android.tools.build:gradle:8.1.0") // 如果需要与Android构建交互
-
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.assertj:assertj-core:3.24.2")
-
-    val group = property("project.groupId").toString()
-    val version = property("project.version").toString()
-
-    api(
-        group = group,
-        name = "base",
-        version = version,
-    )
-
+    val groupId = property("project.groupId").toString()
     rootProject
         .childProjects
         .map { it.value }
@@ -77,9 +24,9 @@ dependencies {
             val artifactId = it.property("project.artifactId").toString()
             if (artifactId == "base") return@forEach
             api(
-                group = "$group.$artifactId",
-                name = "$group.$artifactId.gradle.plugin",
-                version = version,
+                group = "$groupId.$artifactId",
+                name = "$groupId.$artifactId.gradle.plugin",
+                version = it.property("project.version").toString(),
             )
         }
 
